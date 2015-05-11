8f03248a8c1b242caf74c7d206fe0690327ee661
Roger Erens <roger.erens@skin-consultancy.nl>
2015-05-12 00:21:40 +0200

    A working version!

--- main.py
+++ main.py
@@ -3,34 +3,101 @@ from kivy.factory import Factory
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
+# Kivy's install_twisted_rector MUST be called early on!
+from kivy.support import install_twisted_reactor
+install_twisted_reactor()
+
+from autobahn.twisted.wamp import ApplicationSession
+from autobahn.twisted.wamp import ApplicationRunner
+
+from twisted.internet.defer import inlineCallbacks
+
+
+class WampComponent(ApplicationSession):
+    """
+    A WAMP app component run from the Kivy UI.
+    """
+
+    def onJoin(self, details):
+        # print("session ready", self.config.extra)
+
+        # get the Kivy UI component this session was started from
+        ui = self.config.extra['ui']
+        ui.on_session(self)
+
+        # subscribe to WAMP PubSub event and call the Kivy UI component when
+        # events are received
+        self.subscribe(ui.on_vote_message, u'io.crossbar.demo.vote.onvote')
+        self.subscribe(ui.on_reset_message, u'io.crossbar.demo.vote.onreset')
+
 
 class MilkshakeRoot(BoxLayout):
     milkshakes = ObjectProperty(None)
+    milkshake_dict = {}
 
     def add_milkshake(self, name):
         milkshake = Factory.MilkshakeWidget()
         milkshake.name = name
         self.milkshakes.add_widget(milkshake)
+        self.milkshake_dict[name] = self.milkshakes.children[0]
 
-    def reset_numbers(self):
+    @inlineCallbacks
+    def on_session(self, session):
+        """
+        Called from WAMP session when attached to router.
+        """
+        self.session = session
+
+        votes = yield self.session.call(u'io.crossbar.demo.vote.get')
+        for vote in votes:
+            self.milkshake_dict[vote[u'subject']].amount = vote[u'votes']
+
+    def send_vote(self, subject, *args):
+        if self.session:
+            self.session.call(u'io.crossbar.demo.vote.vote', subject)
+
+    def send_reset(self, *args):
+        if self.session:
+            self.session.call(u'io.crossbar.demo.vote.reset')
+
+    def on_vote_message(self, msg):
+        """
+        Called from WampComponent when message was received in a PubSub event.
+        """
+        self.milkshake_dict[msg[u'subject']].amount = msg[u'votes']
+
+    def on_reset_message(self):
+        """
+        Called from WampComponent when message was received in a PubSub event.
+        """
         for milkshake in self.milkshakes.children:
-            milkshake.number_ordered = 0
-
+            milkshake.amount = 0
 
 class MilkshakeWidget(BoxLayout):
-
-    def increase_number(self):
-        self.number_ordered += 1
+    pass
 
 
 class MilkshakeManiaApp(App):
 
     def build(self):
-        root = MilkshakeRoot()
-        milkshake_names = ['banana', 'chocolate', 'lemon']
+
+        self.root = MilkshakeRoot()
+        milkshake_names = [u'Banana', u'Chocolate', u'Lemon']
         for milkshake_name in milkshake_names:
-            root.add_milkshake(milkshake_name)
-        return root
+            self.root.add_milkshake(milkshake_name)
+
+        # WAMP session
+        self.session = None
+
+        # run our WAMP application component
+        # runner = ApplicationRunner(url=u"wss://demo.crossbar.io/ws",
+        #  realm=u"crossbardemo", extra=dict(ui=self.root))
+        runner = ApplicationRunner(url=u'ws://127.0.0.1:8080/ws',
+                                   realm=u'realm1',
+                                   extra=dict(ui=self.root))
+        runner.run(WampComponent, start_reactor=False)
+
+        return self.root
 
 
 if __name__ == '__main__':

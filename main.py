f2bbcbe324370555f08775995e8e1f21cf3432d8
Roger Erens <roger.erens@skin-consultancy.nl>
2015-05-19 01:57:28 +0200

    Change to Votes; add comments/docs

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""
+A Kivy front end component for the WAMP Votes demo application.
+
+The Votes application collects votes for our favourite flavours of ice cream.
+It is powered by a back end WAMP component that runs on the address as
+defined in the start_wamp_component() function.
+See
+https://github.com/crossbario/crossbarexamples/tree/master/votes
+on how to start up your own back end component and Crossbar router.
+
+In this front end component, click a button with an image background to vote
+for the flavour it denotes.
+Reset votes to zero by clicking the bottom button.
+
+Open up a browser window with another front end component to see the numbers
+change.
+"""
+
 # Kivy's install_twisted_rector MUST be called early on!
 from kivy.support import install_twisted_reactor
 install_twisted_reactor()
@@ -12,18 +30,18 @@ from autobahn.twisted.wamp import ApplicationRunner
 from twisted.internet.defer import inlineCallbacks
 
 Builder.load_string('''
-<MilkshakeRoot>:
-    milkshakes: box_with_milkshakes
+<VotesRoot>:
     orientation: 'vertical'
+    votes_container: box_with_vote_widgets
     BoxLayout:
-        id: box_with_milkshakes
+        id: box_with_vote_widgets
         orientation: 'horizontal'
     Button:
         size_hint_y: 0.1
         text: 'Reset'
         on_press: app.root.send_reset()
 
-<MilkshakeWidget@BoxLayout>
+<VoteWidget@BoxLayout>
     orientation: 'vertical'
     name: ''
     amount: 0
@@ -41,9 +59,9 @@ Builder.load_string('''
 ''')
 
 
-class WampComponent(ApplicationSession):
+class VotesWampComponent(ApplicationSession):
     """
-    A WAMP app component run from the Kivy UI.
+    A WAMP application component which is run from the Kivy UI.
     """
 
     def onJoin(self, details):
@@ -53,78 +71,118 @@ class WampComponent(ApplicationSession):
         ui = self.config.extra['ui']
         ui.on_session(self)
 
-        # subscribe to WAMP PubSub event and call the Kivy UI component when
-        # events are received
+        # subscribe to WAMP PubSub events and call the Kivy UI component's
+        # function when such an event is received
         self.subscribe(ui.on_vote_message, u'io.crossbar.demo.vote.onvote')
         self.subscribe(ui.on_reset_message, u'io.crossbar.demo.vote.onreset')
 
 
-class MilkshakeRoot(BoxLayout):
-    milkshakes = ObjectProperty(None)
-    milkshake_dict = {}
+class VotesRoot(BoxLayout):
+    """
+    The Root widget, defined in conjunction with the rule in votes.kv.
+    """
 
-    def add_milkshake(self, name):
-        milkshake = Factory.MilkshakeWidget()
-        milkshake.name = name
-        self.milkshakes.add_widget(milkshake)
-        self.milkshake_dict[name] = self.milkshakes.children[0]
+    votes_container = ObjectProperty(None)  # Used to refer to the widget that
+                                            # holds the VotesWidgets
+    votes = {}  # Since votes_container is a list, we need a dictionary for
+                # bookkeeping, connecting each flavour to its VotesWidget
+
+    def add_vote_widget(self, name):
+        """
+        Dynamically create and add a VoteWidget to the container in Root widget.
+
+        Updates the list self.votes_container and the dictionary self.votes.
+        """
+        vote_widget = Factory.VoteWidget()
+        vote_widget.name = name
+
+        self.votes_container.add_widget(vote_widget)
+
+        self.votes[name] = self.votes_container.children[0]
 
     @inlineCallbacks
     def on_session(self, session):
         """
-        Called from WAMP session when attached to router.
+        Called from WAMP session when attached to Crossbar router.
         """
         self.session = session
 
-        votes = yield self.session.call(u'io.crossbar.demo.vote.get')
-        for vote in votes:
-            self.milkshake_dict[vote[u'subject']].amount = vote[u'votes']
+        # obtain a list of dictionaries with number of votes for each
+        # subject (i.e. flavour)
+        votes_results = yield self.session.call(u'io.crossbar.demo.vote.get')
+        for vote in votes_results:
+            self.votes[vote[u'subject']].amount = vote[u'votes']
 
-    def send_vote(self, subject):
+    def send_vote(self, name):
+        """
+        Called from VoteWidget's top button.
+
+        Only send the name of the flavour that gets an extra vote: the back end
+        will update its number of votes and publish the updated number.
+        """
         if self.session:
-            self.session.call(u'io.crossbar.demo.vote.vote', subject)
+            self.session.call(u'io.crossbar.demo.vote.vote', name)
 
     def send_reset(self):
+        """
+        Called from VotesRoot bottom button.
+        """
         if self.session:
             self.session.call(u'io.crossbar.demo.vote.reset')
 
-    def on_vote_message(self, msg):
+    def on_vote_message(self, vote_result):
         """
-        Called from WampComponent when Crossbar router published vote event.
+        Called from VotesWampComponent when Crossbar router published vote event.
         """
-        self.milkshake_dict[msg[u'subject']].amount = msg[u'votes']
+        self.votes[vote_result[u'subject']].amount = vote_result[u'votes']
 
     def on_reset_message(self):
         """
-        Called from WampComponent when Crossbar router published reset event.
+        Called from VotesWampComponent when Crossbar router published reset event.
         """
-        for milkshake in self.milkshakes.children:
-            milkshake.amount = 0
+        for vote_widget in self.votes_container.children:
+            vote_widget.amount = 0
 
 
-class MilkshakeManiaApp(App):
+class VotesApp(App):
 
     def build(self):
 
-        self.root = MilkshakeRoot()
-        milkshake_names = [u'Banana', u'Chocolate', u'Lemon']
-        for milkshake_name in milkshake_names:
-            self.root.add_milkshake(milkshake_name)
+        self.root = VotesRoot()
 
-        # WAMP session
-        self.session = None
+        flavours = [u'Banana', u'Chocolate', u'Lemon']  # If you adapt this
+                                                        # list, also adapt it
+                                                        # in the back end
+        for flavour in flavours:
+            self.root.add_vote_widget(name=flavour)
 
-        # run our WAMP application component
-        # runner = ApplicationRunner(url=u"wss://demo.crossbar.io/ws",
-        #                            realm=u"crossbardemo",
-        #                            extra=dict(ui=self.root))
-        runner = ApplicationRunner(url=u'ws://127.0.0.1:8080/ws',
-                                   realm=u'realm1',
-                                   extra=dict(ui=self.root))
-        runner.run(WampComponent, start_reactor=False)
+        self.start_wamp_component()
 
         return self.root
 
+    def start_wamp_component(self):
+        """
+        Create a WAMP session and start the WAMP component
+        """
+        self.session = None
+
+        # Alas! demo.crossbar.io needs an older version of autobahn :-(
+        from autobahn import __version__ as v
+        if v < '0.10.2':
+            url, realm = u"wss://demo.crossbar.io/ws", u"crossbardemo"
+        else:
+            # url, realm = u"ws://ws.wolkware.com:19743/ws", u"realm1"
+            url, realm = u"ws://localhost:8080/ws", u"realm1"
+
+        # Create our WAMP application component
+        runner = ApplicationRunner(url=url,
+                                   realm=realm,
+                                   extra=dict(ui=self.root))
+
+        # Start our WAMP application component without starting the reactor because
+        # that was already started by kivy
+        runner.run(VotesWampComponent, start_reactor=False)
+
 
 if __name__ == '__main__':
-    MilkshakeManiaApp().run()
+    VotesApp().run()
